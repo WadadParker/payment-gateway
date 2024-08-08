23652c63432d5cd880b3da6453d299bb3213d2c4
Wadad Parker <wadadparker5@gmail.com>
2024-08-08 07:44:02 +0530

    style: payment gateway

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				bg:{
+				  DEFAULT:'#F2F2F2',
+				  dark: '#BBBBBB',
+				  gray:"#696969"
+				},
+				text:{
+				  DEFAULT:"#2F2F2F",
+				  light:"#ACACAC"
+				},
+				border:{
+				  DEFAULT:"#E1E1E1"
+				}
+			  }
+		},
 	},
 	plugins: [],
 }
