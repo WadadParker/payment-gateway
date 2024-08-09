8446d91064d132ef20e8ed662f72dd52e3358c6d
Wadad Parker <wadadparker5@gmail.com>
2024-08-09 06:19:05 +0530

    Revert "Feat/api (#1)"
    This reverts commit 9f9d97218d131d4229f36a1fdaaeebb634db2d15.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,8 @@
 import { defineConfig } from 'astro/config';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  vite: {
-    resolve: {
-      alias: {
-        buffer: 'buffer'
-      }
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: 'globalThis'
-        },
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true
-          }),
-          NodeModulesPolyfillPlugin()
-        ]
-      }
-    }
-  }
+  integrations: [tailwind()]
 });
