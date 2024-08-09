23cc34b75facb1ea18c8e73084c33a6bc3be6234
Wadad Parker <wadadparker5@gmail.com>
2024-08-09 05:56:42 +0530

    fix: buffer polyfill

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,30 @@
 import { defineConfig } from 'astro/config';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        buffer: 'buffer'
+      }
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis'
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true
+          }),
+          NodeModulesPolyfillPlugin()
+        ]
+      }
+    }
+  }
 });
